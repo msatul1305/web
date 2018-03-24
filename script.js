db8f3284819a6fe74fc8aa49d7ffda69259c0429
Satyajit Mohapatra <smohapatra200@gmail.com>
2018-03-24 23:14:59 +0530

    1st

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+jQuery(document).ready(function ($) {
+    $(window).load(function () {
+        setTimeout(function(){
+            $('#preloader').fadeOut('slow', function () {
+            });
+        },3000); // set the time here
+    });  
+});
+
